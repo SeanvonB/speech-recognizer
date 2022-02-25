a00ef0019ec07a5009e1425d5f90b50fe8cab3d7
SeanvonB <42122062+SeanvonB@users.noreply.github.com>
2022-02-25 13:00:47 -0800

    Combine packages

--- utils.py
+++ utils.py
@@ -2,9 +2,31 @@
 Defines various functions for processing the data.
 """
 import numpy as np
+import os
+import _pickle as pickle
 import soundfile
-from numpy.lib.stride_tricks import as_strided
 from char_map import char_map, index_map
+from data_generator import AudioGenerator
+from keras import backend as K
+from keras.callbacks import ModelCheckpoint   
+from keras.layers import (Input, Lambda)
+from keras.models import Model
+from keras.optimizers import SGD
+from numpy.lib.stride_tricks import as_strided
+
+def add_ctc_loss(input_to_softmax):
+    the_labels = Input(name='the_labels', shape=(None,), dtype='float32')
+    input_lengths = Input(name='input_length', shape=(1,), dtype='int64')
+    label_lengths = Input(name='label_length', shape=(1,), dtype='int64')
+    output_lengths = Lambda(input_to_softmax.output_length)(input_lengths)
+    
+    # CTC Loss implemented in Lambda layer
+    loss_out = Lambda(ctc_lambda_func, output_shape=(1,), name='ctc')(
+        [input_to_softmax.output, the_labels, output_lengths, label_lengths])
+    model = Model(
+        inputs=[input_to_softmax.input, the_labels, input_lengths, label_lengths], 
+        outputs=loss_out)
+    return model
 
 def calc_feat_dim(window, max_freq):
     return int(0.001 * window * max_freq) + 1
@@ -31,6 +53,9 @@ def conv_output_length(input_length, filter_size, border_mode, stride,
         output_length = input_length - dilated_filter_size + 1
     return (output_length + stride - 1) // stride
 
+def ctc_lambda_func(args):
+    y_pred, labels, input_length, label_length = args
+    return K.ctc_batch_cost(labels, y_pred, input_length, label_length)
 
 def spectrogram(samples, fft_length=256, sample_rate=2, hop_length=128):
     """
@@ -137,3 +162,56 @@ def int_sequence_to_text(int_sequence):
         ch = index_map[c]
         text.append(ch)
     return text
+
+def train_model(input_to_softmax, 
+                pickle_path,
+                save_model_path,
+                train_json='train_corpus.json',
+                valid_json='valid_corpus.json',
+                minibatch_size=20,
+                spectrogram=True,
+                mfcc_dim=13,
+                optimizer=SGD(lr=0.02, decay=1e-6, momentum=0.9, nesterov=True, clipnorm=5),
+                epochs=20,
+                verbose=1,
+                sort_by_duration=False,
+                max_duration=10.0):
+    
+    # Create class instance for obtaining batches
+    audio_gen = AudioGenerator(minibatch_size=minibatch_size, 
+        spectrogram=spectrogram, mfcc_dim=mfcc_dim, max_duration=max_duration,
+        sort_by_duration=sort_by_duration)
+
+    # Add training set to generator
+    audio_gen.load_train_data(train_json)
+    audio_gen.load_validation_data(valid_json)
+
+    # Calculate steps_per_epoch
+    num_train_examples=len(audio_gen.train_audio_paths)
+    steps_per_epoch = num_train_examples//minibatch_size
+
+    # Calculate validation_steps
+    num_valid_samples = len(audio_gen.valid_audio_paths) 
+    validation_steps = num_valid_samples//minibatch_size
+    
+    # Add CTC Loss to model specified in input_to_softmax
+    model = add_ctc_loss(input_to_softmax)
+
+    # CTC Loss implemented above; use dummy Lambda function for loss
+    model.compile(loss={'ctc': lambda y_true, y_pred: y_pred}, optimizer=optimizer)
+
+    # Make `results/` directory if necessary
+    if not os.path.exists('results'):
+        os.makedirs('results')
+
+    # Add checkpointer
+    checkpointer = ModelCheckpoint(filepath='results/'+save_model_path, verbose=0)
+
+    # Train the model
+    hist = model.fit_generator(generator=audio_gen.next_train(), steps_per_epoch=steps_per_epoch,
+        epochs=epochs, validation_data=audio_gen.next_valid(), validation_steps=validation_steps,
+        callbacks=[checkpointer], verbose=verbose)
+
+    # Save model loss
+    with open('results/'+pickle_path, 'wb') as f:
+        pickle.dump(hist.history, f)
